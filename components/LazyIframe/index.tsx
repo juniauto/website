7f127616d09f892193b40331bad77ae03289ecec
Wédney Yuri <yuri.lima@zoom.com.br>
2020-07-25 15:53:52 -0300

    components/LazyIframe: check viewport before rendering

--- components/LazyIframe/index.tsx
+++ components/LazyIframe/index.tsx
@@ -1,10 +1,19 @@
 import React, { DetailedHTMLProps, IframeHTMLAttributes } from 'react';
+import dynamic from 'next/dynamic';
+
 
 interface LazyIframeProps extends DetailedHTMLProps<IframeHTMLAttributes<HTMLIFrameElement>, HTMLIFrameElement> {
     loading?: string;
+    height: number;
 };
 
 export default function LazyIframe(props: LazyIframeProps) {
     const Component = 'iframe';
-    return <Component {...props} />
+    const LazyLoad: any = dynamic(() => import('react-lazyload'), {
+        ssr: false,
+        loading: () => <div style={{ height: props.height }} />
+    });
+    return <LazyLoad  height={props.height}>
+        <Component {...props} />
+    </LazyLoad>;
 }
