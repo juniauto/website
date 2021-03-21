d2d25e6ea9187af35d6f8c903e7cb823201d5755
Wédney Yuri Lima <wedneyyuri@gmail.com>
2021-03-21 14:50:01 -0300

    add "not found" page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Head from 'next/head'
+import Link from 'next/link';
 
 import PageContainer from '../components/page/container';
-import ContactUsForm from '../components/contact/contactusform';
-import LazyIframe from '../components/LazyIframe';
 import Whatsapp from '../components/contact/whatsappsection';
-import ContactUs from '../components/contact/contactus';
 import ContactSection from '../components/contact/contactsection';
-import AboutUS from '../components/home/aboutus';
 
 export default function ContactUsPage() {
   return (
@@ -17,13 +14,16 @@ export default function ContactUsPage() {
         <title>Página não encontrada</title>
       </Head>
 
-			<main id="main">
+			<main id="main" style={{ 'paddingTop': '60px' }}>
+
+				<Whatsapp />
+
 				<div id="about" className="about-area area-padding">
 					<div className="container">
 						<div className="row">
 							<div className="col-md-12 col-sm-12 col-xs-12">
 								<div className="section-headline text-center">
-									<h1 className="h2">Auto Socorro 24h Juniauto</h1>
+									<h1 className="h2">Página não encontrada</h1>
 								</div>
 							</div>
 						</div>
@@ -34,7 +34,8 @@ export default function ContactUsPage() {
 										<h4 className="sec-head">Página não encontrada</h4>
 
 										<p>
-											Essa página não existe, em caso de dúvida por favor nos mande uma mensagem.
+											Essa página não existe, você pode <strong>voltar para a página inicial <Link href="/" passHref><a href="/">clicando aqui</a></Link></strong>.
+											Em caso de dúvida por favor nos mande uma mensagem.
                     </p>
 									</div>
 								</div>
@@ -43,6 +44,9 @@ export default function ContactUsPage() {
 
 					</div>
 				</div>
+
+				<ContactSection />
+
 			</main>
 
     </PageContainer>
