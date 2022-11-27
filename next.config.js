7da79bbdd185d30f8ad01c0ccda7a92e4cd97279
Yuri Lima <wedneyyuri@gmail.com>
2022-11-27 14:09:12 -0300

    upgrade packages

--- next.config.js
+++ next.config.js
@@ -1,18 +1,7 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-const withPreact = require('next-plugin-preact');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-module.exports = withPlugins([
-  [withPreact],
-  [optimizedImages, {
-    /* config for next-optimized-images */
-  }],
-
-  // your other plugins here
-
-], {
-  images: {
-    disableStaticImages: true,
-  },
-  productionBrowserSourceMaps: true,
-});
+module.exports = nextConfig
