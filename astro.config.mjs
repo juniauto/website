5af58c21502b257d9bb1bd7d7a6071aba962f072
Yuri Lima <wedneyyuri@gmail.com>
2026-01-17 19:43:10 -0300

    feat: add 404 page, sitemap and robots.txt configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    integrations: [react(), tailwind()],
-    site: 'https://juniauto.com.br',
+    integrations: [react(), tailwind(), sitemap()],
+    site: 'https://www.juniauto.com.br',
+    trailingSlash: 'never',
 });
